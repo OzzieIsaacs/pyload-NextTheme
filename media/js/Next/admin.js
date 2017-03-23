1c2f3f0d3a5cffe4888cec0c04a13f80612ab2ed
OzzieIsaacs <ozzie.fernandez.isaacs@gmail.com>
2017-03-23 22:03:02 +0100

    Bootstrapping of restart dialog Bootstrapping of shutdown dialog Bootstrapping of password change dialog

--- media/js/Next/admin.js
+++ media/js/Next/admin.js
@@ -3,13 +3,13 @@ var root;
 root = this;
 window.addEvent("domready", function() {
     var f, c, b, e, a, d;
-    root.passwordDialog = new MooDialog({
+    /*root.passwordDialog = new MooDialog({
         destroyOnHide: false
     });
     root.passwordDialog.setContent($("password_box"));
     $("login_password_reset").addEvent("click", function(g) {
         return root.passwordDialog.close()
-    });
+    });*/
     $("login_password_button").addEvent("click", function(j) {
         var h, i, g;
         i = $("login_new_password").get("value");
@@ -29,7 +29,8 @@ window.addEvent("domready", function() {
                 }
             });
             h.send();
-            root.passwordDialog.close()
+            $$('#password_box')[0].hide();
+            //root.passwordDialog.close()
         } else {
             alert('{{_("Passwords did not match.")}}')
         }
@@ -41,49 +42,33 @@ window.addEvent("domready", function() {
         f = c.get("id");
         b = f.split("|")[1];
         $("user_login").set("value", b);
-        c.addEvent("click", function(g) {
+        /*c.addEvent("click", function(g) {
             return root.passwordDialog.open()
-        })
+        })*/
     }
     $("quit-pyload").addEvent("click", function(x) {
-        root.QuitDialog = new MooDialog({
-            destroyOnHide: false
-        });
-        root.QuitDialog.setContent($("quit_box"));
-        $("quit_reset").addEvent("click", function(y) {
-            return root.QuitDialog.close()
-        });
         $("quit_button").addEvent("click", function(z) {
             return new Request.JSON({
                 url: "/api/kill",
                 method: "get",
                 onSuccess: function() {
-                    return root.QuitDialog.close();
+                    $$('#quit_box')[0].hide();
                 }
             }).send();
         });
-        root.QuitDialog.open();
     });
 
-    return $("restart-pyload").addEvent("click", function(g) {
-        root.RestartDialog = new MooDialog({
-            destroyOnHide: false
-        });
-        root.RestartDialog.setContent($("restart_box"));
-        $("restart_reset").addEvent("click", function(h) {
-            return root.RestartDialog.close()
-        });
+    $("restart-pyload").addEvent("click", function(g) {
         $("restart_button").addEvent("click", function(j) {
             return new Request.JSON({
                 url: "/api/restart",
                 method: "get",
                 onSuccess: function() {
                     alert("{{_('pyLoad restarted')}}");
-                    return root.RestartDialog.close();
+                    $$('#restart_box')[0].hide();
                 }
             }).send();
         });
-        root.RestartDialog.open();
     });
 }); 
 {% endautoescape %}
